b4b6230f566e8df5875b42bd78f833a90b4bf703
rins <47487086+rins6618@users.noreply.github.com>
2024-07-18 18:04:00 -0300

    Implement of basic functionality

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+// Expressions for dealing with values
+// the actual program logic happens on
+// function definitions below
+
 const add = (op1, op2) => op1 + op2;
 const subtract = (op1, op2) => op1 - op2;
 const multiply = (op1, op2) => op1 * op2;
@@ -6,14 +10,55 @@ const divide = (op1, op2) => {
     return op1 / op2;
 }
 
-const MAX_OPERAND_LENGTH = 11;
-let operand = '0';
-
 const appendDigitToNum = (num, digit) => {
     if (num.length === MAX_OPERAND_LENGTH) return num;
     const numArray = (num === '0') ? [] : num.split('');
     const digitArray = digit.split('');
     return numArray.concat(digitArray).join('');
+}
+
+const MAX_OPERAND_LENGTH = 11;
+let operand = '0';
+let lastOperand = null;
+let currentOperation = null;
+let retypeCheck = false;
+
+// This will be executed if and only if operand, lastOperand and currentOperation are not null;
+function handleOperation() {
+    const operand_num = parseFloat(operand);
+    const lastOpd_num = parseFloat(lastOperand);
+    switch (currentOperation) {
+        case '+':
+            return add(lastOpd_num, operand_num).toString();
+        case '-':
+            return subtract(lastOpd_num, operand_num).toString();
+        case '/':
+            try {
+                return divide(lastOpd_num, operand_num).toString();
+            } catch (e) {
+                return 'ERR: DivBy0'
+            }
+        case '×':
+            return multiply(lastOpd_num, operand_num).toString();
+        default:
+            return handleEquals();
+    }
+}
+
+function handleEquals() {
+    const currentOp = operand;
+    if (lastOperand === null) return operand;
+    if (currentOperation === null) return operand;
+    return handleOperation();
+}
+
+function clearScreen() {
+    const screenObj = document.querySelector("#calculator-screen");
+    const leadingArr = Array(MAX_OPERAND_LENGTH).fill('0');
+    const innerSpan = document.createElement('span');
+    screenObj.textContent = leadingArr.join('');
+    innerSpan.textContent = '';
+    screenObj.appendChild(innerSpan);
 }
 
 function updateScreen() {
@@ -35,6 +80,12 @@ function makeNumpadButtons() {
         const buttonElement = document.createElement("button");
         buttonElement.innerText = button;
         buttonElement.addEventListener("click", _ => {
+            if (retypeCheck) {
+                operand = button;
+                retypeCheck = false;
+                updateScreen();
+                return;
+            }
             operand = appendDigitToNum(operand, button);
             console.log(operand);
             updateScreen();
@@ -50,7 +101,9 @@ function makeOperatorButtons() {
         const buttonElement = document.createElement("button");
         buttonElement.innerText = button;
         buttonElement.addEventListener("click", _ => {
-            //doOperatorLogic(button);
+            currentOperation = button;
+            lastOperand = operand;
+            retypeCheck = true;
         })
         operators.appendChild(buttonElement);
     }
@@ -66,9 +119,13 @@ function makeControlButtons() {
             switch (button) {
                 case 'CL':
                     operand = '0';
-                    updateScreen();
+                    lastOperand = null;
+                    currentOperation = null;
+                    clearScreen();
                     break;
                 case '=':
+                    operand = handleEquals();
+                    updateScreen();
                     break;
             }
         })
