d412a8d0fe699398a9da15ce69b748ec63daa09d
rins <47487086+rins6618@users.noreply.github.com>
2024-07-18 17:12:07 -0300

    Added full calculator design

--- calculator.js
+++ calculator.js
@@ -10,18 +10,15 @@ const MAX_OPERAND_LENGTH = 11;
 let operand = '0';
 
 const appendDigitToNum = (num, digit) => {
-    if (num.length === 11) return num;
-    const numArray = num === '0' ? [] : num.split('');
+    if (num.length === MAX_OPERAND_LENGTH) return num;
+    const numArray = (num === '0') ? [] : num.split('');
     const digitArray = digit.split('');
     return numArray.concat(digitArray).join('');
 }
 
-
-
 function makeNumpadButtons() {
     const numpad = document.querySelector("#calculator-numpad");
     const NUMPAD_BUTTONS = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map(num => num.toString());
-    console.log(NUMPAD_BUTTONS)
     for (let i = 0; i < NUMPAD_BUTTONS.length; i++) {
         let button = NUMPAD_BUTTONS[i];
         const buttonElement = document.createElement("button");
@@ -34,4 +31,33 @@ function makeNumpadButtons() {
     }
 }
 
+function makeOperatorButtons() {
+    const operators = document.querySelector("#calculator-operators");
+    const OPERATOR_BUTTONS = ["+", "-", '/', '×'];
+    for (let i = 0; i < OPERATOR_BUTTONS.length; i++) {
+        let button = OPERATOR_BUTTONS[i];
+        const buttonElement = document.createElement("button");
+        buttonElement.innerText = button;
+        buttonElement.addEventListener("click", _ => {
+            //doOperatorLogic(button);
+        })
+        operators.appendChild(buttonElement);
+    }
+}
+function makeControlButtons() {
+    const control = document.querySelector("#calculator-control");
+    const CONTROL_BUTTONS = ['CL', '='];
+    for (let i = 0; i < CONTROL_BUTTONS.length; i++) {
+        let button = CONTROL_BUTTONS[i];
+        const buttonElement = document.createElement("button");
+        buttonElement.innerText = button;
+        buttonElement.addEventListener("click", _ => {
+            //doOperatorLogic(button);
+        })
+        control.appendChild(buttonElement);
+    }
+}
+
 makeNumpadButtons();
+makeOperatorButtons();
+makeControlButtons();
